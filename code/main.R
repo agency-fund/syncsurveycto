60dfb9f58019e84a470785cfec9b3fb15f21581d
Jake Hughey <jakejhughey@gmail.com>
2024-08-06 20:05:18 +0530

    use rsurveycto from github.

--- code/main.R
+++ code/main.R
@@ -1,4 +1,3 @@
-library('doParallel')
 source(file.path('code', 'utils.R'))
 source(file.path('code', 'sync.R'))
 
@@ -12,15 +11,14 @@ sync_surveycto(scto_params, wh_params)
 
 # high priority:
 # TODO: incremental and deduped sync of form data
-# TODO: github actions cron schedule
 
 # medium priority:
 # TODO: more intelligent fetching of form definitions based on sync_mode
-# TODO: trycatch a la pmparser
-# TODO: logging with https://daroczig.github.io/logger/, to slack?
 # TODO: dev and prod environments, based on branch
+# TODO: trycatch a la pmparser
 
 # low priority:
+# TODO: logging with https://daroczig.github.io/logger/, to slack?
 # TODO: should overwrite be allowed to remove columns or ignore skip criteria?
 # TODO: deal with discrepant column types even if colnames identical
 # TODO: check if review and correction workflow is enabled, and maybe bail
